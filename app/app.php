965ed3b441fc9a459b9a07ac07009e367d2c4a6c
Nicole Freed <nicole.freed@gmail.com>
2017-06-30 15:19:02 -0700

    add /edit_contact and /update_contact handlers to update first contact in contacts list array

--- app/app.php
+++ app/app.php
@@ -29,5 +29,43 @@
         return $app["twig"]->render("contacts_deleted.html.twig");
     });
 
+    $app->post("/edit_contact", function() use ($app) {
+        $contacts_list = Contact::getAll();
+        $current_contact = $contacts_list[0];
+        return $app['twig']->render('edit_contact.html.twig', array('current_contact' => $current_contact));
+    });
+
+    $app->post("/update_contact", function() use ($app) {
+        $contacts_list = Contact::getAll();
+        $current_contact = $contacts_list[0];
+        var_dump($current_contact);
+
+        if (!(empty($_POST['new_first_name']))) {
+            $current_contact->setFirstName($_POST['new_first_name']);
+        }
+        if (!(empty($_POST['new_last_name']))) {
+            $current_contact->setLastName($_POST['new_last_name']);
+        }
+        if (!(empty($_POST['new_street']))) {
+            $current_contact->setStreet($_POST['new_street']);
+        }
+        if (!(empty($_POST['new_city']))) {
+            $current_contact->setCity($_POST['new_city']);
+        }
+        if (!(empty($_POST['new_state']))) {
+            $current_contact->setState($_POST['new_state']);
+        }
+        if (!(empty($_POST['new_zip']))) {
+            $current_contact->setZip($_POST['new_zip']);
+        }
+        if (!(empty($_POST['new_phone']))) {
+            $current_contact->setPhone($_POST['new_phone']);
+        }
+
+        var_dump($current_contact);
+        
+        return $app['twig']->render('update_confirm.html.twig', array('current_contact' => $current_contact));
+    });
+
     return $app;
 ?>
